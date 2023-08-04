3825c4864fe2f6c80902c4f46214abaab9d78c29
Nolan Hawkins <nolanhhawkins@gmail.com>
2023-08-04 13:00:09 -0400

    [jekyll-mapping] Add named locations to jekyll mapping
    With this, post locations can be specified by name rather than by
    specific lat/long, with e.g. `mapping: Vermont, USA, North America`
    
    These locations are converted into specific lat/long pairs using
    the Nominatum OpenStreetMaps Geocoding API - which does not require
    an API key, but should be used concientuously and not abused, so
    please provide an email address and maintain the 2 second query cooldown
    (and don't use this for bulk lookups)
    
    Geocoding results are cached in a tree of locations in the file
    _data/locations.yml, which can additionally be manually edited/corrected
    and where additional place-name aliases can be added for ease of use.
    Nominatum results will never override hand-written results in the locations
    file.

--- _plugins/jekyll_mapping.rb
+++ _plugins/jekyll_mapping.rb
@@ -1,5 +1,14 @@
+require 'open-uri'
+require 'yaml'
+require 'json'
 
 module Jekyll
+    # Location data is retrived from the nominatim OpenStreetmaps geocoding API, whose usage
+    # should be sparing and attached to a specific email address
+    EMAIL = "nolanhhawkins@gmail.com"
+    # Nominatim doesn't like addresses that end in the continent, so let's filter those out from addresses
+    CONTINENTS = ['North America', 'South America', 'Europe', 'Africa', 'Asia', 'Oceania', 'Central America']
+
     class MapTag < Liquid::Tag
         
         def initialize(tag_name, text, tokens)
@@ -72,7 +81,7 @@ module Jekyll
             end
             super
         end
-
+        
         def render(context)
             posts = []
             if @categories
@@ -86,17 +95,23 @@ module Jekyll
             end
             @data['pages'] = []
             for post in posts
-                if post.data['mapping'].has_key?('latitude') && post.data['mapping'].has_key?('longitude')
-                    postinfo = {}
-                    postinfo['title'] = post.data['title']
+                if post.data["mapping"] && (
+                    post.data['mapping'].is_a?(String) ||
+                    (post.data['mapping'].has_key?('latitude') && post.data['mapping'].has_key?('longitude'))
+                )
+                    postinfo = post.data.clone()
                     if Jekyll.configuration({})['baseurl']
-                        postinfo['link'] = "#{Jekyll.configuration({})['baseurl']}#{post.url.chars.first == "/" ? post.url[1..-1] : post.url}"
+                        pogstinfo['link'] = "#{Jekyll.configuration({})['baseurl']}#{post.url.chars.first == "/" ? post.url[1..-1] : post.url}"
                     else
                         postinfo['link'] = post.url
                     end
-                    postinfo['latitude'] = post.data['mapping']['latitude']
-                    postinfo['longitude'] = post.data['mapping']['longitude']
-                    @data['pages'] << postinfo
+                    if post.data['mapping'].is_a?(String) && lookup_location(postinfo, context.registers[:site], post.data['mapping'])
+                      @data['pages'] << postinfo
+                    else
+                      postinfo['latitude'] = post.data['mapping']['latitude']
+                      postinfo['longitude'] = post.data['mapping']['longitude']
+                      @data['pages'] << postinfo
+                    end
                 end
             end
 
@@ -118,6 +133,58 @@ module Jekyll
                     </script>
                     "
             end         
+        end
+
+        # adds the lat/long position data to the postinfo given the location.yml data hierarchy
+        def lookup_location(postinfo, site, location)
+            location_iter = site.data["locations"]
+            name_iter = ''
+            for place in location.split(',').reverse()
+                place = place.strip()
+                foundLocation = false
+                location_iter.each do |key, value| 
+                    if key == place || (value.is_a?(Hash) && value.has_key?('aliases') && value['aliases'].include?(place))
+                        location_iter = value
+                        foundLocation = true
+                        break
+                    end
+                end
+                if !foundLocation
+                    location_iter[place] = {}
+                    location_iter = location_iter[place]
+                end
+                name_iter = CONTINENTS.include?(name_iter) || name_iter == '' ? place:  place + ', ' + name_iter
+
+                if !location_iter.has_key?('latitude')
+                    add_location(location_iter, name_iter, site.data["locations"])
+                end
+            end
+            if location_iter.has_key?('latitude') && location_iter.has_key?('longitude')
+                postinfo['latitude'] = location_iter['latitude']
+                postinfo['longitude'] = location_iter['longitude']
+                return true
+            end
+            return false
+        end
+
+        # adds the lat/long position of a location by name using the nominatim geocoding api
+        def add_location(loc_data, name, all_locations)
+            results = JSON.parse(URI.open(
+                "https://nominatim.openstreetmap.org/search?email=#{EMAIL}&format=json&q=" + name,
+                "User-Agent" => "jekyll-maps.rb/#{EMAIL}"
+            ).read)
+            
+            if results && results[0] && results[0].has_key?('lat') && results[0].has_key?('lon')
+                puts "Found location #{name} #{results[0]}"
+                loc_data['latitude'] = results[0]['lat']
+                loc_data['longitude'] = results[0]['lon']
+                File.write(Dir.pwd + '/_data/locations.yml', all_locations.to_yaml)
+            else
+                puts "Did not find location #{name}"
+            end
+            # Usage of the nominatim API should be limited to no more than one query per second
+            # Sleep for 2 to be extra gentle
+            sleep(2)
         end
     end
 end
